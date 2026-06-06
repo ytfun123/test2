40e71b728acfc30eec76cbb812085d7c9831bc07
ytfun123 <d05269929@gmail.com>
2026-06-05 23:52:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
+app.use(express.static(__dirname));
+
 app.get("/", (req, res) => {
-    res.send("PeerCord server is running");
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on ${PORT}`);
 });
